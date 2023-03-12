14a19c60879469c9e55d706ca22c5b2bf539d8bc
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-03-12 18:36:21 -0300

    feat: handleRemoveRepo
    incrementação da funcionalidade remover

--- src/pages/App.js
+++ src/pages/App.js
@@ -35,8 +35,8 @@ function App() {
 
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
-
-    // utilizar filter.
+      const removerRepo = repos.filter(repo => repo.id !== id)
+      setRepos(removerRepo)
   }
 
 
@@ -45,7 +45,7 @@ function App() {
       <img src={gitLogo} width={72} height={72} alt="github logo"/>
       <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
       <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+      {repos.map(repo => <ItemRepo key={repo.id} handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
     </Container>
   );
 }
